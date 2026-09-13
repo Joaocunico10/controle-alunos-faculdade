f08bac14fa1db6d887563da28378cebde718d3db
Joao Paulo <engs-joaocunico@camporeal.edu.br>
2026-09-13 13:10:06 -0300

    Adicionando User Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserManagementController;
 
 Route::get('/teste-admin', function () {
     return 'Acesso permitido para administrador!';
 })->middleware('auth', 'role:admin');
+
+Route::get('/usuarios', [UserManagementController::class, 'index'])
+    ->middleware(['auth', 'role:admin']);
